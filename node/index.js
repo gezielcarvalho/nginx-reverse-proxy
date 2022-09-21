5f15a387aef8ee635edaf0fc95f83ebb9b604aab
Geziel Carvalho <geziel.natal@gmail.com>
2022-09-21 16:34:21 -0300

    Insert data in mysql from node

--- node/index.js
+++ node/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+const sql = `insert into people(name) values('Sunda')`;
+
+connection.query(sql);
+connection.end();
 
 app.get('/', (req,res) => {
     res.send('<h1>Full Cycle</h1>');
